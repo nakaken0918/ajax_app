425d4c36446e9e0ad4476ca0a20894584c5eb0a0
nakaken0918 <nakaken18.wywy@gmail.com>
2023-05-31 19:31:13 +0900

    リクエスト処理

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,14 +6,11 @@ class PostsController < ApplicationController
 
   end
 
-  # コメントアウトする
-  # def new
-  # end
-
   def create
-
-    Post.create(content: params[:content])
-    redirect_to action: :index  # 追記する
+    # 投稿内容をpostに格納。
+    post = Post.create(content: params[:content])
+    # json:  ...jsonオプション。後に記述されたデータをJSON形式で返信する。
+    render json:{ post: post }
 
   end
 

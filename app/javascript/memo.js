28760fcb93218d068c4282d72541224486aa3a50
nakaken0918 <nakaken18.wywy@gmail.com>
2023-06-02 14:08:11 +0900

    JSでレスポンス処理

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,21 @@
+//投稿メモのHTML
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+
+      <div class="post-content">
+        ${item.content}
+      </div>
+
+    </div>`;
+  return html;
+};
+
 function post (){
   //リクエストを送信する処理
   // getElementById  ...指定した要素を取得。
@@ -15,7 +33,20 @@ function post (){
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
 
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      // buildHTMLで投稿メモのHTMLを外に切り出す。
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+
+    };
   });
  };
 
